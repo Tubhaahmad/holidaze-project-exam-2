0779b74ddd353dd42f9f1a44ec0cd392217f8849
Tubha Ahmad <toubha.ah@hotmail.com>
2026-05-13 15:17:08 +0200

    feat(auth): add register page with Zod validation and Sonner toast #9

---
+++ src/lib/api/auth.ts
@@ -0,0 +1,57 @@
+import { User } from "@/features/auth/store";
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_KEY = process.env.NEXT_PUBLIC_NOROFF_API_KEY;
+
+//the data we send to the API when registering
+export interface RegisterData {
+  name: string;
+  email: string;
+  password: string;
+  venueManager: boolean;
+}
+
+//the data we send to the API when logging in
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+//the shape of ther response we get back from login and register
+//this includes the user profile and the accessToken
+export interface AuthResponse {
+  accessToken: string;
+  name: string;
+  email: string;
+  bio: string;
+  avatar: {
+    url: string;
+    alt: string;
+  };
+  banner: {
+    url: string;
+    alt: string;
+  };
+  venueManager: boolean;
+}
+
+//REGISTER
+
+export async function register(data: RegisterData): Promise<AuthResponse> {
+  const response = await fetch(`${BASE_URL}/auth/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Noroff-API-Key": API_KEY!,
+    },
+    body: JSON.stringify(data),
+  });
+
+  const json = await response.json();
+
+  if (!response.ok) {
+    throw new Error(json.errors?.[0]?.message ?? "Registration failed");
+  }
+
+  return json.data;
+}
